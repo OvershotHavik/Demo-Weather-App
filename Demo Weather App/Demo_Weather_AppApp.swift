25e324da774083bb29f0a466f0a46d867fce78e8
OvershotHavik <plavetzky@gmail.com>
2021-11-09 09:40:23 -0500

    Initial Commit

---
+++ Demo Weather App/Demo_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_Weather_AppApp.swift
+//  Demo Weather App
+//
+//  Created by Steve Plavetzky on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Demo_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demo Weather AppTests/Demo_Weather_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  Demo_Weather_AppTests.swift
+//  Demo Weather AppTests
+//
+//  Created by Steve Plavetzky on 11/9/21.
+//
+
+import XCTest
+@testable import Demo_Weather_App
+
+class Demo_Weather_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
