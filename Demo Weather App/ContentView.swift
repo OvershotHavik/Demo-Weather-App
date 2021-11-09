518e6939686bd66f2d1a57be173d698196f9e64c
OvershotHavik <plavetzky@gmail.com>
2021-11-09 15:28:35 -0500

    Initial UI before refactor

--- Demo Weather App/ContentView.swift
+++ Demo Weather App/ContentView.swift
@@ -10,8 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack{
-//            LinearGradient(gradient: [Color.blue, Color.white], startPoint: .top, endPoint: .bottom)
-            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+            LinearGradient(gradient: Gradient(colors: [.blue, Color("lightBlue")]),
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .ignoresSafeArea()
@@ -20,17 +19,74 @@ struct ContentView: View {
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundColor(.white)
                     .padding()
+                VStack(spacing: 10){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("76°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                .padding(.bottom, 40)
+                HStack(spacing: 25){
+                    WeatherDayView(dayOfWeek: "TUE",
+                                   imageName: "cloud.sun.fill",
+                                   temp: 74)
+                    WeatherDayView(dayOfWeek: "WED",
+                                   imageName: "cloud.sun.rain.fill",
+                                   temp: 78)
+                    WeatherDayView(dayOfWeek: "THU",
+                                   imageName: "wind",
+                                   temp: 68)
+                    WeatherDayView(dayOfWeek: "FRI",
+                                   imageName: "cloud.fog.fill",
+                                   temp: 65)
+                    WeatherDayView(dayOfWeek: "SAT",
+                                   imageName: "sun.max.fill",
+                                   temp: 80)
+                }
                 Spacer()
-                
-                
+                Button {
+                    print("tapped")
+                } label: {
+                    Text("Change Day Time")
+                        .frame(width: 280, height: 50)
+                        .background(.white)
+                        .font(.system(size: 20, weight: .bold, design: .default))
+                        .cornerRadius(10)
+                }
+                Spacer()
             }
         }
-
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherDayView: View {
+    var dayOfWeek: String
+    var imageName: String
+    var temp: Int
+    
+    var body: some View {
+        VStack{
+            Text (dayOfWeek)
+                .font(.system(size: 16, weight: .medium, design: .default))
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 40, height: 40)
+            Text("\(temp)°")
+                .font(.system(size: 28))
+        }
+        .foregroundColor(.white)
     }
 }
